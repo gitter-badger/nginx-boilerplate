62658e0938f7f37978f3aa9a4201841677a2db9a
Michael Gorianskyi <michael.gorianskyi@westwing.de>
2015-12-23 00:34:24 +0100

    Renamed and repositioned files, optimized static files

--- test/main.js
+++ test/main.js
@@ -2,8 +2,8 @@
 var request = require('supertest'),
     express = require('express');
 
-requestNormal = request('http://example.com/');
-requestWww = request('http://www.example.com/');
+requestNormal = request('http://nginx.boilerplate/');
+requestWww = request('http://www.nginx.boilerplate/');
 
 function handle(err) {
     if (err) {
@@ -11,7 +11,8 @@ function handle(err) {
     }
 }
 
-requestNormal.get('/.git').expect(403, handle);
-
-requestNormal.get('/').expect(200, handle);
-requestWww.get('/').expect(301, handle);
+requestWww.get('').expect(301, handle);
+requestNormal.get('.git').expect(403, handle);
+requestNormal.get('').expect(200, handle);
+requestNormal.get('some/random/url/').expect(200, handle);
+requestNormal.get('script.php').expect(200, 'ok');
